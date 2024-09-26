7c68a7f8fd460d5b312ff1a490abe1365bb6883d
Aleksandr <a.gogolev@carbonsoft.ru>
2024-09-26 18:30:25 +0500

    nan

--- chapter2/NaN1.js
+++ chapter2/NaN1.js
@@ -1,4 +1,15 @@
+//NaN - Not a Number
 console.log(parseInt("abc"));   // NaN
 const type = typeof NaN;
 console.log(type);              // number - прикольно
+
+console.log('**************')
+
+const num1 = "javascript";
+const num2 = "22";
+let result = isNaN(num1);
+console.log(result); // true - num1 не является числом
+     
+result = isNaN(num2);
+console.log(result); //  false - num2 - это число
 
